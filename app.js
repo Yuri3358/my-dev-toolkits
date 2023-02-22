a8073dc5fd14054ce32e3f3b3fd56cd171844fc7
Yuri3358 <yurialmeida917@gmail.com>
2023-02-22 14:19:33 -0300

    feat: routes

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const app = Vue.createApp({})
 
+const routes = [
+    {path: "/frameworks", component: frameworks},
+    {path: "/styles", component: styleTools},
+    {path: "/data", component: dataTools},
+]
+
+const router = VueRouter.createRouter({
+    history: VueRouter.createWebHashHistory(),
+    routes
+})
+
 app
 .component("Navbar", headerComponent)
 .component("DataTools", dataTools)
 .component("StyleTools", styleTools)
 .component("Frameworks", frameworks)
+.use(router)
+.mount("#root")
