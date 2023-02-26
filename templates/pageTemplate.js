8cdbcbfb4ae410ece8480592ec03bacf24b86182
Yuri3358 <yurialmeida917@gmail.com>
2023-02-26 16:15:55 -0300

    feat: input

--- templates/pageTemplate.js
+++ templates/pageTemplate.js
@@ -1,9 +1,22 @@
 const pageTemplate = `
-    <div id="styles-box">
+    <header id="brand">
+        <h1>Dev Toolkits</h1>
+    </header>
+
+    <div id="form-box">
+        <label for="tool">Ferramenta</label>
+        <input id="tool">
+        <label for="link">CDN</label>
+        <input id="link">
+        <button class="btn btn-success">Salvar</button>
+    </div>
+    
+    <div id="content-box">
         <ul v-for="tool in this.toolkitList.value">
             <li @click="copyLink(tool.link)">{{ tool.name }}</li>  
         </ul>
     </div>
+
 `
 
 exports = { pageTemplate }
