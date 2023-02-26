601673fe7548983cb53e94218a681e3060e44e9d
Yuri3358 <yurialmeida917@gmail.com>
2023-02-26 16:16:12 -0300

    feat: fetchToolkits()

--- components/pageComponent.js
+++ components/pageComponent.js
@@ -2,23 +2,22 @@ const toolkits = {
     template: pageTemplate,
     data() {
         return {
-            toolkitList: Vue.ref([])
+            toolkitList: Vue.ref([]),
         }
     },
     mounted() {
         this.fetchToolkits()
     },
     methods: {
-        fetchToolkits(filter="framework") {
+        fetchToolkits() {
             let data = [] 
-            colRef.where("type", "==", filter).get()
+            colRef.get()
             .then(snapshot => {
                 snapshot.forEach(doc => {
                     const search = doc.data()
                     data.push(search)
                 })
                 this.toolkitList.value = data
-                console.log(this.toolkitList.value)
             })
         },
         copyLink(tool) {
