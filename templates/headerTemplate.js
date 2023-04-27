1d4423eaced08b483c579ef1deb128d216ef17fa
Yuri3358 <yurialmeida917@gmail.com>
2023-04-27 19:55:36 -0300

    feat: new header template

--- templates/headerTemplate.js
+++ templates/headerTemplate.js
@@ -1,10 +1,12 @@
 const headerTemplate = /*html*/`
-    <div id="header">
-        <header id="header-container">
-            <h1 id="brand">Dev Toolkits</h1>
-            <p id="current-account">{{ accountEmail }}</p>
-        </header>
-    </div>
+    <header id="header-container">
+        <h1 id="brand">Dev Toolkits</h1>
+        <p id="current-account">
+            <a href="#/tools">
+                {{ accountEmail }}
+            </a>
+        </p>
+    </header>
 `
 
 exports = { headerTemplate }
