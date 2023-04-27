3d2b76090996d0e1a7fc4e51074403c99a36a2d1
Yuri3358 <yurialmeida917@gmail.com>
2023-04-27 19:26:08 -0300

    feat: routes

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const app = Vue.createApp({})
+const routes = [
+    {path: "/", component: formComponent},
+    {path: "/tools", component: toolkits},
+]
+
+const router = VueRouter.createRouter({
+    routes,
+    history: VueRouter.createWebHashHistory()
+})
 
 app
-.component("Page", toolkits)
-
+.component("toolkits", toolkits)
+.component("user-form", formComponent)
+.component("app-header", headerComponent)
+.use(router)
 .mount("#root")
