0dc081526931ca0b161e11e92333cfce6f299654
Yuri3358 <yurialmeida917@gmail.com>
2023-04-27 20:02:18 -0300

    style: classes assignment

--- templates/pageTemplate.js
+++ templates/pageTemplate.js
@@ -1,10 +1,12 @@
-const pageTemplate = `
+const pageTemplate = /*html*/`
     <div id="form-box" @keyup.enter="registerTool">
-        <label for="tool">Ferramenta</label>
+        <label class="tools-labels" for="tool">Ferramenta</label>
+        <br/>
         <input id="tool" ref="toolInput" v-model="toolName">
-
-        <label for="link">CDN</label> <br/>
-        <textarea id="link" v-model="linkName"></textarea> <br/>
+        <br/>
+        <label class="tools-labels"for="link">CDN</label>
+        <br/>
+        <textarea id="link" v-model="linkName"></textarea>
         <button class="btn btn-success" @click="registerTool">Salvar Ferramenta</button>
     </div>
 
